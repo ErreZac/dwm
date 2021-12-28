1263c08f2914462144fc83e883580665db788f27
ErreZac <raphael.zaccone@unige.it>
2021-12-28 09:54:34 +0100

    changed some colors

--- config.def.h
+++ config.def.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 18;       /* gap pixel between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx     = 5;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code Nerd Font:size=18" };
 static const char dmenufont[]       = "Fira Code Nerd Font:size=18";
-static const char col_gray1[]       = "#282a36";
+static const char col_gray1[]       = "#bd93f9";
 static const char col_gray2[]       = "#44475a";
 static const char col_gray3[]       = "#6272a4";
 static const char col_gray4[]       = "#f8f8f2";
-static const char col_cyan[]        = "#bd93f9";
+static const char col_cyan[]        = "#282a36";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
