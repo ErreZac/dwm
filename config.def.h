52a0c294442212a9bf9cad78851dba5b48ff0ea7
ErreZac <raphael.zaccone@unige.it>
2022-02-09 10:41:22 +0100

    added tabbed in st spawn command

--- config.def.h
+++ config.def.h
@@ -74,6 +74,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL};//, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[] = { "st", NULL };
+static const char *termtabcmd[] = { "tabbed", "-r", "2", "st", "-w", "''", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *filebrowser[] = { "st", "-e", "ranger", NULL };
@@ -98,7 +99,8 @@ static const char *lockscr[] = { "slock", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	//{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termtabcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = filebrowser } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = email } },
