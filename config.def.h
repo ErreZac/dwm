c1ee18dd657d8571d05bb1015bcd2984dc771974
ErreZac <raphael.zaccone@unige.it>
2022-04-30 13:07:07 +0200

    internal padding of bar

--- config.def.h
+++ config.def.h
@@ -13,6 +13,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 2;    /* horizontal padding for statusbar */
+static const int vertpadbar         = 4;    /* vertical padding for statustbar */
 static const int tagoffset          = 20;
 static const char *fonts[]          = {"Victor Mono Nerd Font:style=Medium:size=20"};
 static const char dmenufont[]       = "Victor Mono Nerd Font:size=20:style:Regular";
