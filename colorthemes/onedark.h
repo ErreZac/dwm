f8a30e1d40183cc9141ea6d64275bdf22bf79184
ErreZac <raphael.zaccone@unige.it>
2022-06-06 12:24:28 +0200

    most dark, now picom manages transpaency

--- colorthemes/onedark.h
+++ colorthemes/onedark.h
@@ -1,14 +1,14 @@
 static const char fg_norm[]     = "#c8ccd4";
-static const char bg_norm[]     = "#0e1013";
+static const char bg_norm[]     = "#040506";
 static const char border_norm[] = "#282c34";
 static const char fg_sel[]      = "#282c34";
 static const char bg_sel[]      = "#c678dd";
 static const char border_sel[]  = "#c678dd";
 static const char fg_txt[]      = "#c8ccd4";
-static const char bg_txt[]      = "#0e1013";
+static const char bg_txt[]      = "#040506";
 static const char border_txt[]  = "#000000";
 static const char fg_st[]       = "#c8ccd4";
-static const char bg_st[]       = "#0e1013";
+static const char bg_st[]       = "#040506";
 static const char border_st[]   = "#000000";
 static const char fg_sys[]      = "#000000";
 static const char bg_sys[]      = "#c678dd";
