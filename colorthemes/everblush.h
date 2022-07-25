531ae5e1f7e38d72de275b9ab88d05e2723e8a45
ErreZac <raphael.zaccone@unige.it>
2022-07-25 17:27:33 +0200

    everblush file

---
+++ colorthemes/everblush.h
@@ -0,0 +1,16 @@
+static const char fg_norm[]     = "#dadada";
+static const char bg_norm[]     = "#141b1e";
+static const char border_norm[] = "#232a2d";
+static const char fg_sel[]      = "#141b1e";
+static const char bg_sel[]      = "#e5c76b";
+static const char border_sel[]  = "#e5c76b";
+static const char fg_txt[]      = "#dadada";
+static const char bg_txt[]      = "#141b1e";
+static const char border_txt[]  = "#000000";
+static const char fg_st[]       = "#dadada";
+static const char bg_st[]       = "#141b1e";
+static const char border_st[]   = "#000000";
+static const char fg_sys[]      = "#000000";
+static const char bg_sys[]      = "#e5c76b";
+static const char border_sys[]  = "#000000";
+
