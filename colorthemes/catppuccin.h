4e8d4bf57402afd111b78b1eb944978cad28ff6c
ErreZac <raphael.zaccone@unige.it>
2023-04-07 08:32:29 +0200

    theming and back to st

--- colorthemes/catppuccin.h
+++ colorthemes/catppuccin.h
@@ -1,16 +1,16 @@
 static const char fg_norm[]     = "#cdd6f4";
-static const char bg_norm[]     = "#11111b";
+static const char bg_norm[]     = "#1e1e2e";
 static const char border_norm[] = "#1e1e2e";
-static const char fg_sel[]      = "#11111b";
+static const char fg_sel[]      = "#1e1e2e";
 static const char bg_sel[]      = "#89b4fa";
 static const char border_sel[]  = "#89b4fa";
 static const char fg_txt[]      = "#cdd6f4";
-static const char bg_txt[]      = "#11111b";
+static const char bg_txt[]      = "#1e1e2e";
 static const char border_txt[]  = "#000000";
 static const char fg_st[]       = "#cdd6f4";
-static const char bg_st[]       = "#11111b";
+static const char bg_st[]       = "#1e1e2e";
 static const char border_st[]   = "#000000";
 static const char fg_sys[]      = "#000000";
-static const char bg_sys[]      = "#89b4fa";
+static const char bg_sys[]      = "#1e1e2e";
 static const char border_sys[]  = "#000000";
 
