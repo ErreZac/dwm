39a23124fac7f77387b08a5aac651f57fdb26999
ErreZac <raphael.zaccone@unige.it>
2023-11-06 14:09:48 +0100

    dawn

---
+++ colorthemes/rosepine_dawn.h
@@ -0,0 +1,16 @@
+static const char fg_norm[]     = "#575279";
+static const char bg_norm[]     = "#f2e9e1";
+static const char border_norm[] = "#f2e9e1";
+static const char fg_sel[]      = "#f2e9e1";
+static const char bg_sel[]      = "#286983";
+static const char border_sel[]  = "#286983";
+static const char fg_txt[]      = "#575279";
+static const char bg_txt[]      = "#f2e9e1";
+static const char border_txt[]  = "#000000";
+static const char fg_st[]       = "#575279";
+static const char bg_st[]       = "#f2e9e1";
+static const char border_st[]   = "#000000";
+static const char fg_sys[]      = "#000000";
+static const char bg_sys[]      = "#f2e9e1";
+static const char border_sys[]  = "#000000";
+
